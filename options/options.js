7d610c758ae187515373e0d986e31b508d6cbee4
Crowdbotics <team@crowdbotics.com>
2023-06-01 03:13:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-rain-41141",
+  url: "https://hidden-rain-41141.botics.co",
+  api: "https://hidden-rain-41141.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
